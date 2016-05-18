12bc148f63f380d8aa757c1c06d07f564e569d4c
Bogdan Klichuk <klichukb@gmail.com>
2016-05-18 17:58:22 +0300

    Implemented sigkey using set.

--- sigkey/sigkey.py
+++ sigkey/sigkey.py
@@ -7,9 +7,9 @@ def task(fl):
     end = ord('z')
     max_n = end - start + 1
     mask = (1 << max_n - 1) - 1
-    seen = set()
     lines = fl.readlines()
     arr_len = len(lines)
+    seen = set()
     array = [None] * arr_len
 
     for i in xrange(arr_len):
@@ -17,28 +17,18 @@ def task(fl):
         for c in lines[i][:-1]:
             key |= 1 << (ord(c) - start)
         array[i] = key
-
     array.sort(reverse=True)
 
+    count = 0
     for i in xrange(arr_len):
-        if i in seen:
-            continue
         x = array[i]
-        left = i + 1
-        right = arr_len - 1
+        if x in seen:
+            seen.remove(x)
+            count += 1
+            continue
         expc = ((1 << x.bit_length()) - 1) ^ x
-        while left <= right:
-            middle = (left + right) // 2
-            y = array[middle]
-            if middle in seen or y < expc:
-                right = middle - 1
-            elif y > expc:
-                left = middle + 1
-            else:
-                seen.add(middle)
-                break
-
-    return len(seen)
+        seen.add(expc)
+    return count
 
 
 def main():
