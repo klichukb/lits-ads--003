072507968b06fdda2bc30da9ee962f844488b656
Bogdan Klichuk <klichukb@gmail.com>
2016-06-11 01:32:27 +0300

    Specialized graph for gamsrv

--- gamsrv/gamsrv.py
+++ gamsrv/gamsrv.py
@@ -1,27 +1,10 @@
+from collections import defaultdict
+
 import heapq
 import sys
 
 INFILE = 'gamsrv.in'
 OUTFILE = 'gamsrv.out'
-
-
-class Vertex(object):
-    def __init__(self, index, out_edges=None):
-        self.index = index
-        self.out_edges = out_edges or []
-
-    def __repr__(self):
-        return 'V({})'.format(self.index)
-
-
-class Edge(object):
-    def __init__(self, start_v, end_v, weight):
-        self.start_v = start_v
-        self.end_v = end_v
-        self.weight = weight
-
-    def __repr__(self):
-        return u'{} - [{}] - {}'.format(self.start_v, self.weight, self.end_v)
 
 
 class Graph(object):
@@ -32,7 +15,7 @@ class Graph(object):
 
 def read_graph(fl):
     vertex_count, edge_count = [int(tok) for tok in fl.readline().split()]
-    vertices = [Vertex(i, []) for i in xrange(vertex_count)]
+    vertices = [set() for i in xrange(vertex_count)]
     client_vertices = {int(tok) - 1 for tok in fl.readline().split()}
     graph = Graph(vertices=vertices, client_vertices=client_vertices)
 
@@ -40,33 +23,30 @@ def read_graph(fl):
         _from, to, weight = [int(tok) for tok in line.rstrip().split()]
         _from -= 1
         to -= 1
-        start_v = graph.vertices[_from]
-        end_v = graph.vertices[to]
-        start_v.out_edges.append(Edge(start_v, end_v, weight))
-        end_v.out_edges.append(Edge(end_v, start_v, weight))
+        graph.vertices[_from].add((to, weight))
+        graph.vertices[to].add((_from, weight))
     return graph
 
 
 def shortest_paths(graph, start_v):
     # Dijkstra for a single vertex.
     distances = [float('inf')] * len(graph.vertices)
-    distances[start_v.index] = 0
+    distances[start_v] = 0
     pq = [(0, start_v)]
     while pq:
         distance, vertex = heapq.heappop(pq)
-        for edge in vertex.out_edges:
-            neighbor = edge.end_v
-            new_distance = distance + edge.weight
-            if new_distance < distances[neighbor.index]:
-                distances[neighbor.index] = new_distance
+        for neighbor, nweight in graph.vertices[vertex]:
+            new_distance = distance + nweight
+            if new_distance < distances[neighbor]:
+                distances[neighbor] = new_distance
                 heapq.heappush(pq, (new_distance, neighbor))
     return distances
 
 
 def max_shortest_paths(graph):
     # Dijkstra for each eligible vertex (non-client).
-    for vertex in graph.vertices:
-        if vertex.index in graph.client_vertices:
+    for vertex in xrange(len(graph.vertices)):
+        if vertex in graph.client_vertices:
             continue
         distances = shortest_paths(graph, vertex)
         yield max(dst for v, dst in enumerate(distances) if v in graph.client_vertices)
