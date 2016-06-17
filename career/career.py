29049ea2f4313117792811ec1b06ddf9bf3d5875
Bogdan Klichuk <klichukb@gmail.com>
2016-06-17 17:53:15 +0300

    Replaced range with xrange since its py2.

--- career/career.py
+++ career/career.py
@@ -8,7 +8,7 @@ def solve(points, max_levels):
     solutions = [0 for i in range(max_levels)]
     solutions[:max_levels] = points[-1]
     for i in xrange(max_levels - 1, -1, -1):
-        for j in range(i):
+        for j in xrange(i):
             solutions[j] = max(solutions[j], solutions[j + 1]) + points[i - 1][j]
     return solutions[0]
 
