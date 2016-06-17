2240189b5c1b68ba1bdf8a4eabf4884c654cf479
Bogdan Klichuk <klichukb@gmail.com>
2016-06-17 04:18:50 +0300

    Optimising lists, summing and default values of solutions

--- ijones/ijones.py
+++ ijones/ijones.py
@@ -14,10 +14,10 @@ def solve(matrix, m, n, targets):
     #
     # P[i] is a map of characters and possible path counts that Mr. Jones can take if he happens
     # to be on the same character on any column on the left of [i].
-    solutions = [None] * m
-    max_size = (ord('z') - ord('a') + 1)
+    solutions = [0 for i in xrange(m)]
     start = ord('a')
-    path_count = [0] * max_size
+    max_size = (ord('z') - start + 1)
+    path_count = [0 for i in xrange(max_size)]
 
     for i in targets:
         solutions[i] = 1
@@ -25,16 +25,18 @@ def solve(matrix, m, n, targets):
 
     interested = {ord(matrix[target][n - 1]) - start for target in targets}
     for j in xrange(n - 2, -1, -1):
-        defer = [0] * max_size
+        # build updates here, apply later.
+        defer = [0 for i in xrange(max_size)]
         for i in xrange(m):
             cell = matrix[i][j]
             cell_ord = ord(cell) - start
             if cell_ord not in interested:
-                if solutions[i] is None:
+                if not solutions[i]:
                     continue
-            if cell == matrix[i][j + 1] or solutions[i] is None:
-                solutions[i] = 0
-            solutions[i] += path_count[cell_ord]
+            if cell == matrix[i][j + 1] or not solutions[i]:
+                solutions[i] = path_count[cell_ord]
+            else:
+                solutions[i] += path_count[cell_ord]
             defer[cell_ord] += solutions[i]
         # apply deferred updates
         for i, value in enumerate(defer):
@@ -42,7 +44,7 @@ def solve(matrix, m, n, targets):
                 interested.add(i)
                 path_count[i] += value
 
-    return sum(solutions[i] for i in range(m) if solutions[i])
+    return sum(solutions)
 
 
 
@@ -50,9 +52,10 @@ def main():
     # read
     with open(INFILE, 'r') as fl:
         n, m = [int(token) for token in fl.readline().split()]
-        matrix = [line.strip() for line in fl.readlines()]
+        matrix = fl.readlines()
 
     targets = (0, m - 1)
+    # NOTE: stripping '\n' in lines relies only on value of `m` passed to solve().
     result = solve(matrix, m, n, targets)
 
     # write
